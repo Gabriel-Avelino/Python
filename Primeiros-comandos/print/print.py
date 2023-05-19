12e470568beadb76a225979ede63d009b3727f96
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-19 16:47:55 -0300

    Início do curso de python: aula 04

---
+++ Primeiros-comandos/print/print.py
@@ -0,0 +1,8 @@
+#Para o Python, todos os comandos são funções. A estrutura para escrever algo na tela em Python é a seguinte:
+print('Hello')
+
+#Ao apresentar um número, não usamos aspas:
+print(7)
+
+#Para fazer uma soma, basta utilizar o operador:
+print(7+4) 

a7ddce3c5ad04160983f14e9747784388544d462
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-23 15:46:50 -0300

    Ex-006 e Ex-007

---
+++ mundo-01/Exercicios-Mundo-01/ex-006.py
@@ -0,0 +1,3 @@
+num = float(input('Digite um número: '))
+
+print('O dobro de {0} é: {1} \nO triplo de {0} é: {2} \nA raiz quadrada de {0} é: {3}'.format(num, (num * 2), (num * 3), (num**(1/2))))
---
+++ mundo-01/Exercicios-Mundo-01/ex-007.py
@@ -0,0 +1,5 @@
+media = float(input('Digite a primeira nota:'))
+
+media += float(input('Digite a segunda nota: '))
+
+print('A média final do aluno é: {}'.format(media/2))

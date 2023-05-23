36cfb6785ff7a01f18423abef66667538d2f0292
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-23 15:49:31 -0300

    Ex-007

--- mundo-01/Exercicios-Mundo-01/ex-007.py
+++ mundo-01/Exercicios-Mundo-01/ex-007.py
@@ -2,4 +2,4 @@ media = float(input('Digite a primeira nota:'))
 
 media += float(input('Digite a segunda nota: '))
 
-print('A média final do aluno é: {}'.format(media/2))
+print('A média final do aluno é: {:.2f}'.format(media/2))

d637d404cc8bca3764b4f4199f2aa92525e1b7a8
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-25 15:32:34 -0300

    Aula-08: Mudando nome da pasta de músicas

--- mundo-01/Aulas/Aula-08:Utilizando-Módulos/desafios/musica.py
+++ mundo-01/Aulas/Aula-08:Utilizando-Módulos/desafios/musica.py
@@ -1,5 +1,5 @@
 import os
-os.chdir("./musicas-desafio16")
+os.chdir("musicas-desafio21")
 file = input("Digite a música que você deseja tocar: ")
 
 print('Tocando "{}" usando player nativo'.format(file))

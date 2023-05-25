023b4f65d4b4cf7ff921017e39a0477f2c27d49d
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-25 11:49:53 -0300

    Ex-016

--- mundo-01/Exercicios/ex-016.py
+++ mundo-01/Exercicios/ex-016.py
@@ -2,4 +2,4 @@ from math import trunc
 
 num = float(input('Digite um número: '))
 
-print('O número digitado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
+print('O número digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))

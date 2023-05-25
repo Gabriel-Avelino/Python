caf0f0834344107c763cb2d18211ee31f7224c7a
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-25 11:45:22 -0300

    Aula-08: Corrigindo erro do arquivo musica

---
+++ mundo-01/Aulas/Aula-08:Utilizando-Módulos/desafios/musica.py
@@ -0,0 +1,4 @@
+import os
+file = "videoplayback.mp3"
+print('Tocando "Comigo Estás" de Paulo Baruk usando player nativo')
+os.system("afplay " + file)
--- mundo-01/Aulas/Aula-08:Utilizando-Módulos/desafios/musica/musica.py
+++
@@ -1,4 +0,0 @@
-import os
-file = "videoplayback.3gp"
-print('Tocando "Comigo Estás" de Paulo Baruk usando o Player Nativo')
-os.system("afplay " + file)

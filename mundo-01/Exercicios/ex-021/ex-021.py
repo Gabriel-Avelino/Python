bf2b6fa130e111d03dbe4e9d5905304f23cc8e5b
Gabriel Avelino <avelinogabrieldossantos@gmail.com>
2023-05-25 19:59:02 -0300

    Update ex-021.py

--- mundo-01/Exercicios/ex-021/ex-021.py
+++ mundo-01/Exercicios/ex-021/ex-021.py
@@ -3,7 +3,7 @@ import pygame
 
 pygame.init()
 
-musica = input('Digite o nome do arquivo de áudio com sua extensão: ')
+musica = input('Digite o nome do arquivo de áudio: ')
 
 print('Tocando "{}" usando player pygame'.format(musica))
 
@@ -11,7 +11,7 @@ print('Tocando "{}" usando player pygame'.format(musica))
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 
 # Concatene o caminho absoluto com o nome do arquivo
-caminho_arquivo = os.path.join(diretorio_atual,'musicas-desafio21', musica)
+caminho_arquivo = os.path.join(diretorio_atual,'musicas-desafio21', '{}.mp3'.format(musica))
 
 pygame.mixer.music.load(caminho_arquivo)
 pygame.mixer.music.play()

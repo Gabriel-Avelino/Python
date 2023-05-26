00f77883abe9ef01e1d1cf750556e096d492d31e
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-26 15:57:52 -0300

    Aula-09: Ajuste desafio frase

--- mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/frase.py
+++ mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/frase.py
@@ -1,10 +1,12 @@
 from unicodedata import normalize
 
 frase = input('Digite uma frase: ').strip()
-normalizada = normalize('NFD', frase)
+normalizada = normalize('NFD', frase).encode('ascii', 'ignore').decode('ascii')
+
+print(normalizada)
 
 print('''A frase digitada foi: {}
 Quantas vezes aparece a letra A: {}
-Em que posição ela aparece primeiro?: {}
-Em que posição ela aparece pela última vez: {}
-'''.format(frase, normalizada.upper().count('A'), normalizada.upper().find('A'), normalizada.upper().rfind('A')))
+Em que posição ela aparece primeiro?: {}ª
+Em que posição ela aparece pela última vez: {}ª
+'''.format(frase, normalizada.upper().count('A'), normalizada.upper().find('A')+1, normalizada.upper().rfind('A')+1))

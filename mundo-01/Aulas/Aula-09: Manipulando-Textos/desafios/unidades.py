8058a61d6b8adf897ae4481b2f118bd33205ba7e
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-26 14:22:02 -0300

    Aula-09: desafio unidades corrigido

--- mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/unidades.py
+++ mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/unidades.py
@@ -1,7 +1,7 @@
-num = int(input('Digite um número inteiro entre 0 e 9999: ')).strip()
+num = int(input('Digite um número inteiro entre 0 e 9999: '))
 
 print('''O número digitado foi {}
 Unidade: {}
 Dezena: {}
 Centena: {}
-Milhar {}'''.format(num, separado[0], separado[1], separado[2], separado[3]))
+Milhar {}'''.format(num, num//1 % 10, num//10 % 10, num//100 % 10, num//1000 % 10))

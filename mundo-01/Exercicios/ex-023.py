bcfe7864a66182bafb30a8d1f52ba821cd66b77c
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-26 14:17:38 -0300

    Ex-023: Corrigido

--- mundo-01/Exercicios/ex-023.py
+++ mundo-01/Exercicios/ex-023.py
@@ -1,4 +1,3 @@
-num = input('Digite um número entre 0 e 9999: ')
-unidades = num.replace('', ' ').split()
+num = int(input('Digite um número entre 0 e 9999: '))
 
-print('Analisando o número {0} \nUnidade: {4} \nDezena: {3} \nCentena: {2} \nMilhar: {1}'.format(num, unidades[0], unidades[1], unidades[2], unidades[3]))
+print('Analisando o número {} \nUnidade: {} \nDezena: {} \nCentena: {} \nMilhar: {}'.format(num, num//1 % 10, num//10% 10, num//100 % 10, num//1000 % 10))

e3f8dfc3516f1b2e1fe2e017d02e5a3e991a98f8
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-26 14:19:47 -0300

    Ex-023: Completo

--- mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/unidades.py
+++ mundo-01/Aulas/Aula-09: Manipulando-Textos/desafios/unidades.py
@@ -1,8 +1,7 @@
-num = input('Digite um número inteiro entre 0 e 9999: ').strip()
+num = int(input('Digite um número inteiro entre 0 e 9999: ')).strip()
 
-separado = num.replace('', ' ').split()
-print('''O número digitado foi {0}
-Unidade: {4}
-Dezena: {3}
-Centena: {2}
-Milhar {1}'''.format(num, separado[0], separado[1], separado[2], separado[3]))
+print('''O número digitado foi {}
+Unidade: {}
+Dezena: {}
+Centena: {}
+Milhar {}'''.format(num, separado[0], separado[1], separado[2], separado[3]))

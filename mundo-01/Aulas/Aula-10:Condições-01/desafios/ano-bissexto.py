54e5d27d11d1d67bc1df8729301446cc338b8f20
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-30 10:33:26 -0300

    Aula-09: Ajuste desafio ano-bissexto

--- mundo-01/Aulas/Aula-10:Condições-01/desafios/ano-bissexto.py
+++ mundo-01/Aulas/Aula-10:Condições-01/desafios/ano-bissexto.py
@@ -1,6 +1,6 @@
 ano = int(input('Digite o ano desejado: '))
 
-if ano % 4 == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 100 == 0 and ano % 400 == 0:
     print('O ano {} é bissexto.'.format(ano))
 else:
     print('O ano {} não é bissexto.'.format(ano))

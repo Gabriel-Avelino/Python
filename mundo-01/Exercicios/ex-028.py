454c5d7170a664e490843d86723f7734fff7e5d6
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-05-30 08:33:23 -0300

    Ex-028: Ajustes

--- mundo-01/Exercicios/ex-028.py
+++ mundo-01/Exercicios/ex-028.py
@@ -1,10 +1,12 @@
 from random import randint
 
 num = randint(0, 5)
-print('-=-' * 30, '\nVou pensar em um número entre 0 e 5. Tente adivinhar...\n', '-=-' * 30)
+print('-=-' * 30, '\nVou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 30)
 
 palpite = int(input('Em que número pensei? '))
 print('PROCESSANDO...')
+
 if num == palpite:
     print('PARABÉNS! Você conseguiu me vencer!')
 else:

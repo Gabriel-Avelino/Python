966b83fc35e86743329fa727212185e25facae78
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-05 16:54:06 -0300

    ex-045 incompleto

---
+++ mundo-02/Exercícios/ex-045.py
@@ -0,0 +1,12 @@
+from random import choice
+from time import sleep
+game = ['PEDRA', 'PAPEL', 'TESOURA']
+
+jogador = str(input('''\033[36mSuas opções:
+\033[34m[ 0 ] \033[33mPEDRA
+\033[34m[ 1 ] \033[33mPAPEL
+\033[34m[ 2 ] \033[33mTESOURA
+\033[37mQual é a sua jogada? '''))
+computador = choice(game)
+print('\033[31mJO')
+if

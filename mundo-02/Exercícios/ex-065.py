3cb95aa288b24106d632c8568cad23759bfe1327
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-16 11:24:16 -0300

    Ex-065: corrigido

--- mundo-02/Exercícios/ex-065.py
+++ mundo-02/Exercícios/ex-065.py
@@ -4,7 +4,7 @@ maior = 0
 menor = 0
 opcao = ''
 
-while opcao != 'N':
+while opcao in 'S':
     num = float(input('Digite um número: '))
     soma += num
     c += 1
@@ -13,16 +13,12 @@ while opcao != 'N':
     if num < menor:
         menor = num
 
-    while opcao not in ['S', 's', 'N', 'n']:
-        opcao = str(input('Deseja continuar? [S/N] ')).upper()
-        if opcao == 'SIM':
-            opcao = 'S'
-        if opcao == 'NÃO':
-            opcao = 'N'
-        if opcao not in 'SsNn':
+    while opcao not in ['S', 'N']:
+        opcao = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if opcao not in 'SN':
             print('Digite uma opção válida')
     if opcao == 'S':
         opcao = ''
 media = soma / c
-print('''Você digitou  {} números e a média foi {:.2f}
+print('''Você digitou {} números e a média foi {:.2f}
 O maior valor foi {} e o menor foi {}'''.format(c, media, maior, menor))

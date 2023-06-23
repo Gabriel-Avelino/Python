97a817171d2739368dd0f7e04ede49d9ae9f3537
Gabriel Avelino <avelinogabrieldossantos@gmail.com>
2023-06-23 15:43:08 -0300

    Delete mundo-03/Exercícios directory

--- mundo-03/Exercícios/ex-074.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-nums = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
-
-print('Os valores sorteados foram: ', end='')
-for pos, n in enumerate(nums):
-    print(nums[pos], end=' ')
-
-print(f'\nO maior valor sorteado foi {max(nums)}')
-print(f'O menor valor sorteado foi {min(nums)}')

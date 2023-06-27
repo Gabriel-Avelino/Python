5df929fc4153a2868dbe59f1e6ca9f988ddc5af7
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-27 08:24:54 -0300

    Aula 17: final

--- mundo-03/Aulas/Aula-17_Listas-parte-01/desafios/expressões.py
+++ mundo-03/Aulas/Aula-17_Listas-parte-01/desafios/expressões.py
@@ -1,2 +1,9 @@
-expressao = str(input('Digite a expressão: ')).strip().split()
-print(expressao)
+expressao = str(input('Digite a expressão: ')).strip().replace('', ' ').split()
+direito = expressao.count(')')
+esquerdo = expressao.count('(')
+total = esquerdo + direito
+
+if total % 2 == 0 and esquerdo == direito:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')

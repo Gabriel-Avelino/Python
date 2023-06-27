e20d799b4b3f67d05e7570651ad4d4b920b47e29
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-27 14:24:44 -0300

    Aula-17: Correção desafio de expressões numéricas

--- mundo-03/Aulas/Aula-17_Listas-parte-01/desafios/expressões.py
+++ mundo-03/Aulas/Aula-17_Listas-parte-01/desafios/expressões.py
@@ -1,9 +1,17 @@
 expressao = str(input('Digite a expressão: ')).strip().replace('', ' ').split()
-direito = expressao.count(')')
-esquerdo = expressao.count('(')
-total = esquerdo + direito
+pilha =[]
 
-if total % 2 == 0 and esquerdo == direito:
+for simb in expressao:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
     print('Sua expressão está válida!')
 else:
     print('Sua expressão está errada!')

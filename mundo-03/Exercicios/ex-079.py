0a95c4b54371c7ddcac600a5205fd183f2e80b02
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-27 10:37:39 -0300

    Ex-079

---
+++ mundo-03/Exercicios/ex-079.py
@@ -0,0 +1,18 @@
+valores = []
+
+while True:
+    num = int(input('Digite um valor: '))
+    if num not in valores:
+        valores.append(num)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+
+    continuar = ''
+    while continuar not in ['S', 'N']:
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+valores.sort()
+print('-=' * 30)
+print(f'Você digitou os valores {valores}')

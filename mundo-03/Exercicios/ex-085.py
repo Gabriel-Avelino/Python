b57207ac0f3ba93d806622cbb06517ff0de80b8a
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-30 08:20:29 -0300

    Ex-085

---
+++ mundo-03/Exercicios/ex-085.py
@@ -0,0 +1,15 @@
+nums = [[], []]
+
+for c in range(1, 8):
+    n = int(input(f'Digite o {c}o. valor: '))
+
+    if n % 2 == 0:
+        nums[0].append(n)
+    else:
+        nums[1].append(n)
+
+nums[0].sort()
+nums[1].sort()
+
+print(f'''Os valores pares digitados foram: {nums[0]}
+Os valores ímpares digitados foram: {nums[1]}''')

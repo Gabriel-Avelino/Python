204276ccd5f6a649ad80d1d6e8862cd91bc5c462
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-30 16:41:08 -0300

    Ex-087

---
+++ mundo-03/Exercicios/ex-087.py
@@ -0,0 +1,32 @@
+matriz = []
+linha = []
+somaPares = 0
+somaColuna3 = 0
+maiorNumLinha2 = 0
+
+for c in range(0, 3):
+    for i in range(0, 3):
+        linha.append(int(input(f'Digite um valor para [{c}, {i}]: ')))
+    matriz.append(linha[:])
+    linha.clear()
+
+print('-=' * 30)
+
+for c, l in enumerate(matriz):
+    for i, e in enumerate(l):
+        print(f'[{e:^5}]', end=' ')
+    print()
+
+print('-=' * 30)
+
+for c, l in enumerate(matriz):
+    for i, e in enumerate(l):
+        if e % 2 == 0:
+            somaPares += e
+        if c == 1 and e > maiorNumLinha2:
+            maiorNumLinha2 = e
+    somaColuna3 += l[2]
+
+print(f'''A soma dos valores pares é {somaPares}.
+A soma dos valores da terceira coluna é {somaColuna3}.
+O maior valor da segunda linha é {maiorNumLinha2}.''')

95c20fdc72db12dcda520c05c2454926d3ed5aef
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-30 09:56:29 -0300

    Ex-086: Modificações

--- mundo-03/Exercicios/ex-086.py
+++ mundo-03/Exercicios/ex-086.py
@@ -1,11 +1,8 @@
-matriz = []
-linha = []
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 for c in range(0, 3):
     for i in range(0, 3):
-        linha.append(int(input(f'Digite um valor para [{c}, {i}]: ')))
-    matriz.append(linha[:])
-    linha.clear()
+        matriz[c][i] = int(input(f'Digite um valor para [{c}, {i}]: '))
 
 print('-=' * 30)
 

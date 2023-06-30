c1fe6f84d849612be15c530aae46ab0decc2a630
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-06-30 09:33:48 -0300

    Aula 18: Correção Matriz

--- mundo-03/Aulas/Aula-18_Listas-parte-02/desafios/matriz.py
+++ mundo-03/Aulas/Aula-18_Listas-parte-02/desafios/matriz.py
@@ -10,7 +10,7 @@ print('-=' * 30)
 for c, l in enumerate(matriz):
     for i, e in enumerate(l):
         if i == 0 and c > 0:
-            print(f'\n[ {l[i]} ]', end=' ')
+            print(f'\n{f"[ {e:^6} ]":^6}', end=' ')
         else:
-            print(f'[ {l[i]} ]', end=' ')
+            print(f'{f"[ {e:^6} ]":^6}', end=' ')
 

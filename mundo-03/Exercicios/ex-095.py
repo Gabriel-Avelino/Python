281507462e5c402db2961367cb2b35462adb462f
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-10 16:55:34 -0300

    Ex-095 parte 1

---
+++ mundo-03/Exercicios/ex-095.py
@@ -0,0 +1,29 @@
+jogadores = list()
+ficha = dict()
+gols = []
+
+while True:
+    ficha['nome'] = str(input('Nome do Jogador: ')).strip().title()
+    quantidade = int(input(f'Quantas partidas {ficha["nome"]} jogou? '))
+
+    for c in range(0, quantidade):
+        gols.append(int(input(f'    Quantos gols na partida {c + 1}? ')))
+    ficha['gols'] = gols[:]
+    gols.clear()
+    ficha['total'] = sum(ficha['gols'])
+    jogadores.append(ficha.copy())
+    ficha.clear()
+
+    continuar = ''
+    while continuar not in ['S', 'N']:
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if continuar not in ['S', 'N']:
+            print('ERRO! Responda apenas S ou N.')
+    if continuar == 'N':
+        break
+
+print('-=' * 30)
+print(f'{"cod":<4}{"nome":<10}{"gols":<10}{"total":>10}')
+print('-' * 35)
+for i, j in enumerate(jogadores):
+    print(f'{i:>3} {j["nome"]:<8}  {str(j["gols"]):<8}{j["total"]:>8}')

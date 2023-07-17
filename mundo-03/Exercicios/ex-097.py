7a904e51cb641e762b2afa7d9201dfc4e197c185
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-17 11:04:20 -0300

    Ex-097

---
+++ mundo-03/Exercicios/ex-097.py
@@ -0,0 +1,10 @@
+def escreva(text):
+    tamanho = len(text) + 4
+    print('~' * tamanho)
+    print(f'  {text}')
+    print('~' * tamanho)
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')

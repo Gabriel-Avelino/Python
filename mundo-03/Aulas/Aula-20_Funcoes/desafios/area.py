0f6e460c5caeb5d527a4d4a0ab2f5a2a21386b3a
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-17 08:15:36 -0300

    Aula 19: Desafio de área correção

--- mundo-03/Aulas/Aula-20_Funcoes/desafios/area.py
+++ mundo-03/Aulas/Aula-20_Funcoes/desafios/area.py
@@ -3,7 +3,8 @@ def area(l, c):
     print(f'A área de um terreno {l}x{c} é de {a}m².')
 
 
-print(f'{"Controle de Terrenos"}')
+print(f'{"Controle de Terrenos":^30}')
+print('-' * 30)
 largura = float(input('LARGURA (m): '))
 comprimento = float(input('COMPRIMENTO (m): '))
 area(largura, comprimento)

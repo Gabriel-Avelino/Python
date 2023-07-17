7d2d09ebb086cbb365c047701f1c99d79dcdb1c8
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-17 12:41:42 -0300

    Ex-100

---
+++ mundo-03/Exercicios/ex-100.py
@@ -0,0 +1,25 @@
+from time import sleep
+from random import randint
+numeros = []
+
+
+def sorteia(lista):
+    for c in range(0, 5):
+        lista.append(randint(1, 10))
+    print('Sorteando 5 valores da lista: ', end='')
+    for v in numeros:
+        print(v, end=' ')
+        sleep(.5)
+    print('PRONTO!')
+
+
+def somapar(lista):
+    total = 0
+    for n in lista:
+        if n % 2 == 0:
+            total += n
+    print(f'Somando os valores pares de {lista}, temos {total}')
+
+
+sorteia(numeros)
+somapar(numeros)

71d81438d89586593893a8ff139f2994851b5085
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-20 12:37:05 -0300

    Ex-108

---
+++ mundo-03/Exercicios/ex-108/teste.py
@@ -0,0 +1,7 @@
+import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda.formatar(p)} é {moeda.formatar(moeda.metade(p))}')
+print(f'O dobro de {moeda.formatar(p)} é {moeda.formatar(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos {moeda.formatar(moeda.aumentar(p, 10))}')
+print(f'Diminuindo 13%, temos {moeda.formatar(moeda.diminuir(p, 13))}')

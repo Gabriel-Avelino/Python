f3bd8e0afa77e80f2c98f85ee78e8619f9bf9c4e
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-20 09:56:43 -0300

    Ex-107

---
+++ mundo-03/Exercicios/ex-107/moeda.py
@@ -0,0 +1,16 @@
+def dobro(value):
+    return value * 2
+
+
+def metade(value):
+    return value / 2
+
+
+def aumentar(value, por):
+    value += (por / 100) * value
+    return value
+
+
+def diminuir(value, por):
+    value -= (por / 100) * value
+    return value

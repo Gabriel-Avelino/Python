0794c16e5ea26e1a6da487f45246ac802124f470
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-21 12:50:25 -0300

    Aula-23: Prática e Teoria

---
+++ mundo-03/Aulas/Aula-23_Erros-e-Excecoes/teoria/erros-excecoes.py
@@ -0,0 +1,21 @@
+# O acontecimento de erros e exceções é algo comum no dia a dia.
+# Um erro ocasionado por um comando que normalmente funcionaria é chamado de exceção
+# Existem erros e exceções de vários tipos. Entre eles temos:
+
+# primt  # Erro de sintaxe
+
+# print(x)  # Variável não declarada
+
+# print(int('Número: '))  # Erro de tipo de valor
+
+# print(5 / 0)  # Divisão por zero
+
+# print(2/'2')  # Erro de tipo
+
+"""lst =[3, 6, 4]
+print(lst[3])"""  # Erro de index
+
+"""ficha ={'nome': 'João'}
+print(ficha['idade'])"""  # Erro de chave
+
+# import uteis  # Módulo não encontrado

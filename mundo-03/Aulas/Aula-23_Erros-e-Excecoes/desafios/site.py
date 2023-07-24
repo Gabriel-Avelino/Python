40736994c7853a905866aad57566229899c7d746
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-24 09:59:31 -0300

    Aula-23: Correção do desafio de verificação do acesso de um site / Formatando arquivo

--- mundo-03/Aulas/Aula-23_Erros-e-Excecoes/desafios/site.py
+++ mundo-03/Aulas/Aula-23_Erros-e-Excecoes/desafios/site.py
@@ -2,6 +2,6 @@ import requests
 try:
     resp = requests.get('http://www.pudim.com.br')
 except:
-        print(f'\033[31mO site pudim não está acessível no momento.\033[m')
+    print(f'\033[31mO site pudim não está acessível no momento.\033[m')
 else:
     print(f'\033[32mConsegui acessar o site pudim com sucesso!\033[m')
